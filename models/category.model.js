13d50922435439c25a71ae9fe58c989e84de1ae1
NguyenLam05200 <holohoi05200@gmail.com>
2020-12-11 04:38:40 +0700

    .

--- models/category.model.js
+++ models/category.model.js
@@ -1,25 +1,3 @@
-const {
-    db
-} = require('../utils/db');
-const {
-    ObjectId
-} = require('mongodb');
-
-const mongoose = require("mongoose");
-
-const nameCollection = 'Categories';
-
-const schemaCategory = new mongoose.Schema({
-    name: String
-});
-
-// compile schema to model
-const Category = mongoose.model(nameCollection, schemaCategory);
-
-//code cứng khi database k có collection Categories
-
-
-
 module.exports = {
     getAll: async () => {
         //code cứng:
@@ -33,42 +11,5 @@ module.exports = {
             }
         ];
         return list;
-    },
-    getOne: async (id) => {
-        const catCollection = db().collection(nameCollection);
-        const one = await catCollection.findOne({
-            _id: ObjectId(id)
-        })
-        return one;
-    },
-    addOne: async (data) => {
-        var newCat = new Category({
-            name: data.CatName
-        });
-        //console.log(newCat);
-        const catCollection = db().collection(nameCollection);
-        return await catCollection.insertOne(newCat);
-    },
-    patchOne: async (data) => {
-        const catCollection = db().collection(nameCollection);
-        return await catCollection.updateOne({
-            "_id": ObjectId(data._id)
-        }, {
-            $set: {
-                "name": data.name
-            }
-        });
-    },
-    delOne: async (data) => {
-        const catCollection = db().collection(nameCollection);
-        var result = null;
-        try {
-            result = await catCollection.deleteOne({
-                "_id": ObjectId(data._id)
-            });
-        } catch (e) {
-            console.log(e);
-        }
-        return result;
     }
 }
